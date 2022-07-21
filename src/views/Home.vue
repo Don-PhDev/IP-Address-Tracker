6d892f6723f00b1ce34868b2dcd737e5b0d85a4e
Don Forrest <forrest@philippinedev.com>
2022-07-21 23:38:25 +0800

    Initialize Search input

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="home">
+  <div class="flex flex-col h-screen max-h-screen">
+    <div
+      class="flex justify-center relative bg-hero-bg bg-cover px-4 pt-8 pb-32"
+    >
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">
+          IP Address Tracker
+        </h1>
+        <div class="flex">
+          <input
+            class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+            type="text"
+            placeholder="Search for IP address or leave empty to get your IP info"
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
