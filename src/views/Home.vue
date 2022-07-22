075453829145ae2d47f9f17e84facc595f08376a
Don Forrest <forrest@philippinedev.com>
2022-07-22 22:40:34 +0800

    Leaflet map implementation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col h-screen max-h-screen">
     <div
-      class="flex justify-center relative bg-hero-bg bg-cover px-4 pt-8 pb-32"
+      class="z-20 flex justify-center relative bg-hero-bg bg-cover px-4 pt-8 pb-32"
     >
       <div class="w-full max-w-screen-sm">
         <h1 class="text-white text-center text-3xl pb-4">
@@ -23,16 +23,34 @@
           ></i>
         </div>
       </div>
+      <IPInfo />
     </div>
+    <div id="map" class="h-full z-10"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import IPInfo from '../components/IPInfo.vue'
+import leaflet from "leaflet"
+import { onMounted } from 'vue'
 
 export default {
   name: 'Home',
   components: {
-  }
+    IPInfo
+  },
+  setup() {
+    let map
+
+    onMounted(() => {
+      map = leaflet.map('map').setView([51.505, -0.09], 13)
+
+      leaflet
+        .tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '© OpenStreetMap'
+        }).addTo(map)
+    })
+  },
 }
 </script>
